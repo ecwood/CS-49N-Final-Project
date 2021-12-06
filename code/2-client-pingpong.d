3f4329389643172875fc00a58b3cdb5ab1bbf415
Erica Wood <ericacwood@gmail.com>
2021-12-05 22:50:56 -0800

    administrative

--- code/2-client-pingpong.d
+++
@@ -1,9 +0,0 @@
-2-client-pingpong.o: 2-client-pingpong.c \
- /home/ewood/CS49N/libpi/include/rpi.h \
- /home/ewood/CS49N/libpi/include/gpio.h \
- /home/ewood/CS49N/libpi/include/../libc/libc-extra.h \
- /home/ewood/CS49N/libpi/include/../libc/assert.h \
- /home/ewood/CS49N/libpi/include/rpi-constants.h nrf.h nrf-config.h \
- /home/ewood/CS49N/libpi/libc/circular.h \
- /home/ewood/CS49N/libpi/include/rpi-interrupts.h \
- /home/ewood/CS49N/libpi/include/rpi.h nrf-test.h
--- code/ads1115_joystick.d
+++
@@ -1,8 +0,0 @@
-ads1115_joystick.o: ads1115_joystick.c \
- /home/ewood/CS49N/libpi/include/rpi.h \
- /home/ewood/CS49N/libpi/include/gpio.h \
- /home/ewood/CS49N/libpi/include/../libc/libc-extra.h \
- /home/ewood/CS49N/libpi/include/../libc/assert.h \
- /home/ewood/CS49N/libpi/include/rpi-constants.h i2c.h ads1115_joystick.h \
- /home/ewood/CS49N/libpi/libc/bit-support.h \
- /home/ewood/CS49N/libpi/libc/helper-macros.h
